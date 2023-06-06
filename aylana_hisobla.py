c54dccd901713a42fd116d31ab834196b0c05bfa
yusufraxmanov <134388216+yusufraxmanov@users.noreply.github.com>
2023-06-06 23:44:07 +0500

    Add files via upload

---
+++ aylana_hisobla.py
@@ -0,0 +1,15 @@
+#Berilgan oraliqdagi tub sonlar ro'yxatini qaytaruvchi 
+#funksiya yozing (tub sonlar —faqat birga va o'ziga 
+#qoldiqsiz bo'linuvchi, 1 dan katta musbat sonlar)
+
+def tub_son(num1,num2):
+    """Oraliqdagi tub sonlarni shakllantiruvchi funksiya"""
+    in range(num1,num2)
+
+
+
+
+
+
+
+
---
+++ son_katta_konsol_chiq.py
@@ -0,0 +1,23 @@
+#Foydalanuvchidan aylaning radiusini qabul qilib olib, 
+#uning radiusini, diametrini, uzunligi va yuzini 
+#lug'at ko'rinishida qaytaruvchi funksiya yozing
+
+def aylana_hisobla(num):
+    """Aylana diametri, perimetri va yuzini hisoblovchi funksiya"""
+    print(f"Radiusi - {num}\n"
+          f"Diametri - {num*2}\n"
+          f"Uzunligi - {2*num*3.14}\n"
+          f"Yuzi - {3.14*(num**2)}")
+
+aylana_hisobla(36)
+        
+
+
+
+
+
+
+
+
+
+
